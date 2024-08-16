d395bded38c50c16b2a738f89970ab219cca15a4
mohametalmeari <mohamet.almeari@gmail.com>
2024-08-16 21:30:22 +0300

    Create admin-access-only endpoint

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 
 const authentication = require("./authentication");
+const admin = require("./admin");
 
 const router = Router();
 
 module.exports = () => {
   authentication(router);
+  admin(router);
   return router;
 };
